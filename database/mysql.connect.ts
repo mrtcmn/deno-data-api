4581b0def708d7ee2dcdcb0d363ef070aebb6ec6
Murat Çimen <mrtcmen@gmail.com>
2022-11-16 22:30:12 +0300

    change: mysql driver setup

---
+++ database/mysql.connect.ts
@@ -0,0 +1,28 @@
+import * as mysql2 from "https://deno.land/x/mysql2/mod.ts";
+
+
+export const MySqlClient = () => {
+    let client;
+
+    function getMySqlConnection() {
+        client = mysql2.createPool({
+            host: "127.0.0.1",
+            port: 3306,
+            user: "root",
+            password: "testPass",
+            database: "dataSet",
+            connectionLimit: 4,
+        });
+
+    }
+    function get() {
+        if (!client) {
+            getMySqlConnection();
+        }
+        return client;
+    }
+    return {
+        get
+    }
+
+}
