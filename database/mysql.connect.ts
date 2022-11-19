e3a6ac5016ab59f10a171640efdd6b01777b3dc0
Murat Çimen <mrtcmen@gmail.com>
2022-11-19 16:20:41 +0300

    change: mysql driver environmental settings apply

--- database/mysql.connect.ts
+++ database/mysql.connect.ts
@@ -1,17 +1,23 @@
 import * as mysql2 from "https://deno.land/x/mysql2/mod.ts";
 
+const DB_HOST = Deno.env.get("DB_HOST");
+const DB_USER = Deno.env.get("DB_USER");
+const DB_DATABASE = Deno.env.get("DB_DATABASE");
+const DB_PASSWORD = Deno.env.get("DB_PASSWORD");
 
 export const MySqlClient = () => {
     let client;
 
     function getMySqlConnection() {
         client = mysql2.createConnection({
-            host: "localhost",
+            host: DB_HOST,
             port: 3306,
-            user: "root",
-            password: "testPass",
-            database: "test",
+            user: DB_USER,
+            password: DB_PASSWORD,
+            database: DB_DATABASE,
         });
+
+        console.error("Connected to MySQL",DB_HOST,DB_USER);
 
     }
     function get() {
