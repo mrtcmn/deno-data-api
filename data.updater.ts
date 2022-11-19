baccf3817437a25492fc94c0055c255090f759cc
Murat Çimen <mrtcmen@gmail.com>
2022-11-19 19:32:49 +0300

    fix: drop table command add for less buggy init method

--- data.updater.ts
+++ data.updater.ts
@@ -20,21 +20,21 @@ export const init = async () => {
         const client = await MySqlClient().get();
 
         let initialTableSetting = await client.execute(`
-            create table if not exists dataSet
-                (
-                    id int auto_increment primary key,
-                    event_time    datetime                     null,
-                    event_type    varchar(255) charset utf8mb3 null,
-                    product_id    varchar(255) charset utf8mb3 null,
-                    category_code varchar(255) charset utf8mb3 null,
-                    category_id   varchar(255) charset utf8mb3 null,
-                    brand         varchar(255) charset utf8mb3 null,
-                    price         int                          null,
-                    user_id       varchar(255) charset utf8mb3 null,
-                    user_session  varchar(255) charset utf8mb3 null
-                );
-
-            `)
+            drop table if exists dataSet;
+            create table dataSet
+            (
+                id            int auto_increment primary key,
+                event_time    datetime                     null,
+                event_type    varchar(255) charset utf8mb3 null,
+                product_id    varchar(255) charset utf8mb3 null,
+                category_code varchar(255) charset utf8mb3 null,
+                category_id   varchar(255) charset utf8mb3 null,
+                brand         varchar(255) charset utf8mb3 null,
+                price         int                          null,
+                user_id       varchar(255) charset utf8mb3 null,
+                user_session  varchar(255) charset utf8mb3 null
+            );
+`)
 
         let newData = [...data];
         let query = `
